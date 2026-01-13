750f3324323a076cca312fe9d47e57a7f2404d66
nretrorsum <daniel0629692@gmail.com>
2026-01-13 13:39:09 +0200

    Oliynyk test programs update

--- oliynyk/output.c
+++ oliynyk/output.c
@@ -8,31 +8,31 @@
 
 int main() {
     // Variable declarations (int_2 = int16_t)
+    int16_t var_A = 0;
+    int16_t var_B = 0;
     int16_t var_I = 0;
-    int16_t var_SUM = 0;
-    int16_t var_N = 0;
-    int16_t var_F = 0;
+    int16_t var_J = 0;
+    int16_t var_X = 0;
+    int16_t var_SQR = 0;
     
-    scanf("%hd", &var_N);
-    var_I = 1;
-    var_SUM = 0;
-    while ((var_I < (var_N + 1))) {
-        var_SUM = (var_SUM + var_I);
+    scanf("%hd", &var_A);
+    scanf("%hd", &var_B);
+    var_I = var_A;
+    while ((var_I < (var_B + 1))) {
+        var_SQR = (var_I * var_I);
+        printf("%d\n", var_SQR);
         var_I = (var_I + 1);
     }
-    printf("%d\n", var_SUM);
-    scanf("%hd", &var_N);
-    var_F = 1;
+    var_X = 0;
     var_I = 1;
-    while ((var_I < (var_N + 1))) {
-        var_F = (var_F * var_I);
+    while ((var_I < (var_A + 1))) {
+        var_J = 1;
+        while ((var_J < (var_B + 1))) {
+            var_X = (var_X + 1);
+            var_J = (var_J + 1);
+        }
         var_I = (var_I + 1);
     }
-    printf("%d\n", var_F);
-    var_I = 5;
-    while ((var_I > 0)) {
-        printf("%d\n", var_I);
-        var_I = (var_I - 1);
-    }
+    printf("%d\n", var_X);
     return 0;
 }
